066012ffd5db2ee244b70f9c5c81e46055dd9280
Andreas Work <awork@ruc.dk>
2020-04-21 15:10:21 +0200

    added HandlePrintRequest

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,9 @@
 package sample;
         import javafx.application.Platform;
+        import javafx.scene.control.TextArea;
+
+        import java.util.Stack;
+
 public class Controller {
     private  final AdjacencyGraph model;
     private  final DijkstraFormView view;
@@ -8,5 +12,23 @@ public class Controller {
         this.view = DijkstraView;
         view.exitBtn.setOnAction(e-> Platform.exit());
         view.DijkstraBtn.setOnAction(e->model.Dijkstra(view.startVertexComB.getValue()));
+    }
+
+    public void HandlePrintRequest(Vertex destination, TextArea TArea) {
+        Vertex pvertex = destination;
+        TArea.appendText("To " + destination.Name + " Shortest length: " + destination.distance);
+        Stack<Vertex> Path = new Stack<>();
+        int limit =0;
+        while (pvertex!=null)
+        {
+            Path.push(pvertex);
+            pvertex = pvertex.prev;
+        }
+        if (!Path.isEmpty())
+            limit = Path.size();
+        for (int i=0; i<limit -1; i++)
+            TArea.appendText(Path.pop().Name + " -> ");
+        if (limit >0)
+            TArea.appendText(Path.pop().Name);
     }
 }
